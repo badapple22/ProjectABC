3e8d19de499e65efc9c48a89f94dabe98951c1ab
badapple22 <badapple22277@gmail.com>
2024-12-21 17:52:44 +0900

    a44c44 added

--- GitProject2/src/GitProject/FileC.java
+++ GitProject2/src/GitProject/FileC.java
@@ -5,7 +5,7 @@ public class FileC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello C");
-		System.out.println("Hello C4");
+		System.out.println("Hello C44");
 
 
 	}
